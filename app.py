d024accd0825b312ac0ce6e126b8edad506958bb
tenteniaa <teniafebrianti@students.unnes.ac.id>
2024-01-13 08:23:11 +0700

    finally

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 
 import pandas as pd
-import numpy as np
 import pickle
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model_LR = pickle.load(open('model.pkl', 'rb'))
+le = pickle.load(open('le.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+pca = pickle.load(open('pca.pkl', 'rb'))
 
 @app.route('/')
 def index():
@@ -24,61 +23,58 @@ def contact():
 
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    return render_template('form2.html')
 
-@app.route('/form/prediksi', methods=["GET", "POST"])
+@app.route('/form/prediksi')
 def predict():
-    try:
-        if request.method == "POST":
-            # Mendapatkan data dari request POST
-            data = request.json
+    return render_template('prediksi.html')
 
-            # Menyesuaikan data dengan feature engineering yang telah diterapkan pada data training
-            data['Age_Category'] = 0
-            if (data['Age'][0] >= 16) & (data['Age'][0] <= 25):
-                data['Age_Category'] = 1
-            elif (data['Age'][0] > 25) & (data['Age'][0] <= 35):
-                data['Age_Category'] = 2
-            elif (data['Age'][0] > 35) & (data['Age'][0] <= 45):
-                data['Age_Category'] = 3
-            elif (data['Age'][0] > 45) & (data['Age'][0] <= 55):
-                data['Age_Category'] = 4
-            elif (data['Age'][0] > 55) & (data['Age'][0] <= 65):
-                data['Age_Category'] = 5
-            elif (data['Age'][0] > 65) & (data['Age'][0] <= 75):
-                data['Age_Category'] = 6
-            elif (data['Age'][0] > 75) & (data['Age'][0] <= 85):
-                data['Age_Category'] = 7
-            elif data['Age'][0] > 85:
-                data['Age_Category'] = 8
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        form_values = request.form.to_dict()
 
-            data.pop('Age')
-            data['PolyInteraction'] = data['Polyuria'][0] * data['Polydipsia'][0]
+        data = {}
 
-            # Konversi ke DataFrame
-            data_df = pd.DataFrame(data)
+        for key, value in form_values.items():
+            try:
+                new_value = float(value)
+                data[key] = [new_value]
+            except ValueError:
+                print(f"Warning: Nilai '{value}' pada atribut '{key}' tidak dapat diubah menjadi float.")
 
-            # le = LabelEncoder()
-            # df['class'] = le.fit_transform(df['class'])
+        # Lakukan sesuatu dengan data, misalnya, prediksi menggunakan model machine learning
+        data['Age_Category'] = 0
+        if (data['Age'][0] >= 16) & (data['Age'][0] <= 25):
+            data['Age_Category'] = 1
+        elif (data['Age'][0] > 25) & (data['Age'][0] <= 35):
+            data['Age_Category'] = 2
+        elif (data['Age'][0] > 35) & (data['Age'][0] <= 45):
+            data['Age_Category'] = 3
+        elif (data['Age'][0] > 45) & (data['Age'][0] <= 55):
+            data['Age_Category'] = 4
+        elif (data['Age'][0] > 55) & (data['Age'][0] <= 65):
+            data['Age_Category'] = 5
+        elif (data['Age'][0] > 65) & (data['Age'][0] <= 75):
+            data['Age_Category'] = 6
+        elif (data['Age'][0] > 75) & (data['Age'][0] <= 85):
+            data['Age_Category'] = 7
+        elif data['Age'][0] > 85:
+            data['Age_Category'] = 8
+        data.pop('Age')
+        data['PolyInteraction'] = data['Polyuria'][0] * data['Polydipsia'][0]
 
-            # Pilih hanya kolom numerik untuk transformasi
-            numeric_columns = data_df.select_dtypes(include=[np.number]).columns
-            data_numeric = data_df[numeric_columns]
+        data_df = pd.DataFrame(data)
 
-            # Menerapkan transformasi PCA pada data baru
-            scaler = StandardScaler()
-            new_data_std = scaler.transform(data_numeric)
-            pca = PCA(n_components=14)
-            new_data_pca = pca.transform(new_data_std)
+        new_data_std = scaler.transform(data_df)
+        new_data_pca = pca.transform(new_data_std)
 
-            # Prediksi terhadap data baru
-            prediction = model.predict(new_data_pca)
-            predicted_class = prediction[0]
-            # predicted_class_original = le.inverse_transform([predicted_class])[0]
+        # Prediksi terhadap data baru
+        prediction = model_LR.predict(new_data_pca)
+        predicted_class = prediction[0]
+        predicted_class_original = le.inverse_transform([predicted_class])[0]
 
-            return render_template('prediksi.html', prediction_text = "{}".format(predicted_class))
+        return render_template('prediksi.html', prediction_text=predicted_class_original)
 
-        return render_template('prediksi.html', prediction_text="Negative")
-        
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    # Jika metode bukan POST, tampilkan halaman formulir
+    return render_template('form2.html')
